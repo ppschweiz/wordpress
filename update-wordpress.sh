f2b0fb7730aad79a6707e08c3479b80faded2f82
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-09-26 17:20:22 +0200

    cleanup

--- update-wordpress.sh
+++ update-wordpress.sh
@@ -4,8 +4,6 @@ set -e
 set -u
 
 WEB_PATH=/var/www/html
-WP_PATH=$(ls -1 $WEB_PATH | grep -v lost.found | tail -n1)
-FULL_WP_PATH="$WEB_PATH/$WP_PATH"
 BACKUP_DIR=/var/backup/wordpress
 DELETE_LIST="wp-content wp-config.php"
 
@@ -14,7 +12,7 @@ DELETE_LIST="wp-content wp-config.php"
 #
 function check_version {
         version_new=$(grep '^\$wp_version' wp-includes/version.php | grep -o '[0-9.]\+')
-        version_old=$(grep '^\$wp_version' $FULL_WP_PATH/wp-includes/version.php | grep -o '[0-9.]\+')
+        version_old=$(grep '^\$wp_version' $WEB_PATH/wp-includes/version.php | grep -o '[0-9.]\+')
         if [ $version_new -le $version_old ]
         then
                 cleanup
@@ -35,7 +33,7 @@ function update {
         rm -Rf $DELETE_LIST
         chown -R "$APACHE_RUN_USER:$APACHE_RUN_GROUP" .
 
-        rsync -a . $FULL_WP_PATH
+        rsync -a . $WEB_PATH
 }
 
 update
