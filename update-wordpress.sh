4c871278507167421eebc99e12071ea464687556
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-09-26 17:11:00 +0200

    use wordpress from docker image

--- update-wordpress.sh
+++ update-wordpress.sh
@@ -6,7 +6,6 @@ set -u
 WEB_PATH=/var/www/html
 WP_PATH=$(ls -1 $WEB_PATH | grep -v lost.found | tail -n1)
 FULL_WP_PATH="$WEB_PATH/$WP_PATH"
-TMP_DIR=$(mktemp -d)
 BACKUP_DIR=/var/backup/wordpress
 DELETE_LIST="wp-content wp-config.php"
 
@@ -25,25 +24,10 @@ function check_version {
 }
 
 #
-# make a backup of the current version, keep 3 versions
-#
-function backup {
-        mkdir -p $BACKUP_DIR
-        tar -czf $BACKUP_DIR/wordpress-$(date +%F.%s).tar.gz $FULL_WP_PATH
-        cd $BACKUP_DIR
-        ls -1t $BACKUP_DIR | sed -n '4,$ p' | xargs rm -f
-}
-
-#
 # do the update
 #
 function update {
-        cd $TMP_DIR
-        wget https://wordpress.org/latest.zip
-        unzip latest.zip
-        wget https://i18n.svn.wordpress.org/fr_FR/$(sed -n 's/^\(\d\.\d\).*/\1/' $version_new)/*.mo
-        wget https://i18n.svn.wordpress.org/de_DE/$(sed -n 's/^\(\d\.\d\).*/\1/' $version_new)/*.mo
-        cd wordpress
+        cd /usr/src/wordpress/
 
         # exit if version is uptodate
         check_version
@@ -54,15 +38,5 @@ function update {
         rsync -a . $FULL_WP_PATH
 }
 
-#
-# cleanup all temp stuff
-#
-function cleanup {
-        rm -Rf $TMP_DIR
-}
-
-
-backup
 update
-cleanup
 
